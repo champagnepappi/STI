a3bcb82e16cda8b3583bb7f4fb65baeeb948cd71
kevin <kevinochieng548@gmail.com>
2017-03-30 13:09:10 +0300

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330095949) do
+ActiveRecord::Schema.define(version: 20170330100432) do
+
+  create_table "lecturers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string   "name"
